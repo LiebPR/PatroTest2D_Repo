af31f02473e84d8617013b7595a27ccda4553081
AinhoaRB9 <ainhoa23.rb@gmail.com>
2024-12-13 20:10:55 +0100

    13/12
    Flip
    1 Salto

--- Test2D24-25/Assets/Test2D_Rut/Scrpts/PlayerController2D.cs
+++ Test2D24-25/Assets/Test2D_Rut/Scrpts/PlayerController2D.cs
@@ -12,10 +12,14 @@ public class PlayerController2D : MonoBehaviour
 
     [Header("Movement Parameters")]
     public float speed;
+    [SerializeField] bool isFacingRight;
 
     [Header("Jump Parameters")]
     public float jumpForce;
-    [SerializeField]bool isGorunded;
+    [SerializeField] bool isGrounded;
+    [SerializeField] GameObject groundCheck;
+    [SerializeField] float groundCheckRadius;
+    [SerializeField] LayerMask groundLayer;
 
 
 
@@ -25,12 +29,15 @@ public class PlayerController2D : MonoBehaviour
         //Para autoreferenciar: nombre de variable = GetComponent<tipo de variable>()
         playerRb=GetComponent<Rigidbody2D>();
         playerInput=GetComponent<PlayerInput>();
+        isFacingRight = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GroundCheck();
+        if (moveInput.x > 0 && !isFacingRight) Flip();
+        if (moveInput.x < 0 && isFacingRight) Flip();
     }
 
     private void FixedUpdate()
@@ -41,6 +48,20 @@ public class PlayerController2D : MonoBehaviour
     void Movement()
     {
         playerRb.velocity = new Vector3(moveInput.x * speed, playerRb.velocity.y, 0);
+    }
+
+    void Flip()
+    {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
+        isFacingRight = !isFacingRight;
+    }
+
+    void GroundCheck()
+    {
+        //isGrounded es verdadero cuando el circulo detector toque la layer ground
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, groundCheckRadius, groundLayer);
     }
 
     #region Input Methods
@@ -57,7 +78,11 @@ public class PlayerController2D : MonoBehaviour
     {
         if (context.started)
         {
-            playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            if (isGrounded)
+            {
+                playerRb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+            }
+            
         }
         
     }
